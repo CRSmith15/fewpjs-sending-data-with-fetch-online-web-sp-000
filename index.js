3df353c47522902f5ccc2a7e582be3e4968c8324
Chance Smith <chancesmith97@hotmail.com>
2020-09-11 21:24:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+function submitData(name, email){
+  let configObj = {
+    headers: {
+      "Content-Type" : "application/json"
+      "Accept" : "application/json"
+    },
+    userData = {
+      name: `${name}`,
+      email: `${email}`
+    };
+    body: JSON.stringify(userData)
+  };
+
+} 
